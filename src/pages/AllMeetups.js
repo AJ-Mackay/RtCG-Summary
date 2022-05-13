f5b6208d81ab30ea53c9f773d89a78a22056572d
Ash Mackay <ash.mackay85@gmail.com>
2022-05-13 15:05:44 +0100

    Using the useEffect() hook

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import MeetupList from "../components/meetups/MeetupList";
 
@@ -6,16 +6,30 @@ function AllMeetupsPage() {
   const [isLoading, setIsLoading] = useState(true);
   const [loadedMeetups, setLoadedMeetups] = useState([]);
 
-  fetch(
-    "https://react-summary-27ea0-default-rtdb.europe-west1.firebasedatabase.app/meetups.json"
-  )
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      setIsLoading(false);
-      setLoadedMeetups(data);
-    });
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(
+      "https://react-summary-27ea0-default-rtdb.europe-west1.firebasedatabase.app/meetups.json"
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setIsLoading(false);
+        const meetups = [];
+
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            ...data[key],
+          };
+
+          meetups.push(meetup);
+        }
+
+        setLoadedMeetups(meetups);
+      });
+  }, []);
 
   if (isLoading) {
     return (
