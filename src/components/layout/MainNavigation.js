1dacb5ff464dabeb535f6c47e40dea8acfcfaf4a
Ash Mackay <ash.mackay85@gmail.com>
2022-05-13 15:57:21 +0100

    More Context Usage

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,8 +1,13 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+
+import FavouritesContext from "../../store/favourites-context";
 
 import classes from "./MainNavigation.module.css";
 
 function MainNavigation() {
+  const favouritesCtx = useContext(FavouritesContext);
+
   return (
     <header className={classes.header}>
       <div className={classes.logo}>React Meetups</div>
@@ -15,7 +20,12 @@ function MainNavigation() {
             <Link to="/new-meetup">Add New Meetup</Link>
           </li>
           <li>
-            <Link to="/favourites">My Favourites</Link>
+            <Link to="/favourites">
+              My Favourites
+              <span className={classes.badge}>
+                {favouritesCtx.totalFavourites}
+              </span>
+            </Link>
           </li>
         </ul>
       </nav>
